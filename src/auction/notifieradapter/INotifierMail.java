2cf7eea12b4559a8c70ae66d4889881d3826e7ff
Lucian-MihaiStan <stanlucianmihai@yahoo.com>
2021-02-09 21:05:56 +0200

    javadoc

--- src/auction/notifieradapter/INotifierMail.java
+++ src/auction/notifieradapter/INotifierMail.java
@@ -7,12 +7,46 @@ import products.Product;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Email notifier interface
+ * <pre>
+ *     The email is sent from auctionhouseroyal@gmail.com
+ * </pre>
+ */
 public interface INotifierMail {
+    /**
+     * Send mail to function
+     * @param email email address of recipient
+     * @param textMail text of email
+     */
     void sendEmail(String email, String textMail);
+
+    /**
+     * Send mail to brokers to notify them the auction started and their clients distributed
+     * @param mapBrokers brokers
+     * @param auction auction that is started
+     * @param product product info that is included in auction
+     */
     void sendMailToBrokers(Map<Integer, List<Pair<String, Double>>> mapBrokers, Auction auction, Product product);
+
+    /**
+     * Send mail to winner of auction
+     * @param winnerEmail email of the winner
+     * @param productInfo information of product
+     */
     void sendWinnerEmail(String winnerEmail, Product productInfo);
 
+    /**
+     * Send mail to participant auction started and was won by someone
+     * @param email email of user
+     * @param idAuction id auction
+     */
     void toPWasWon(String email, int idAuction);
 
+    /**
+     * Send mail to participant auction started, but wasn't win
+     * @param email email of user
+     * @param idAuction id auction
+     */
     void toPNotWon(String email, int idAuction);
 }
